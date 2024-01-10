c1ba6e86caec883a404f5fbf0a2bbf965fa340b8
huishanyi <1342088650@qq.com>
2024-01-10 15:11:10 +0800

    引入 element-plus UI 组件库，配置按需导入，添加全局配置

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import type { ElButton } from 'element-plus';
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    <ElButton type="primary">
+      点击
+    </ElButton>
     <TheWelcome />
   </main>
 </template>
