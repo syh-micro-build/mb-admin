2ecaffb08aeb293ed55c9d4d01f9cd3e6c9c8d6c
huishanyi <1342088650@qq.com>
2024-01-11 09:19:32 +0800

    路由结构调整

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+export const constantRoutes = [
+  {
+    path: '/',
+    component: HomeView,
+    name: 'Root',
+    meta: {
+      hidden: true
+    }
+  },
+  // {
+  //   path: '/login',
+  //   component: () => import('@/views/Login/Login.vue'),
+  //   name: 'Login',
+  //   meta: {
+  //     hidden: true,
+  //     title: '登录',
+  //     noTagsView: true
+  //   }
+  // }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: constantRoutes,
+  scrollBehavior: () => ({ left: 0, top: 0 })
 })
 
 export default router
