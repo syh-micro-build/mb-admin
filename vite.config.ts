a222d341cff09e58d6c8252193fdfb38a00f1caa
huishanyi <1342088650@qq.com>
2024-01-15 15:11:28 +0800

    添加 mock 功能

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import UnoCSS from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    UnoCSS(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build'
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      env.VITE_USE_MOCK === 'true'
+        ? viteMockServe({
+            mockPath: 'mock',
+            enable: !isBuild
+          })
+        : undefined,
+      UnoCSS(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    optimizeDeps: {
+      include: [
+        'vue-types',
+        '@zxcvbn-ts/core',
+      ]
     }
-  },
-  optimizeDeps: {
-    include: [
-      'vue-types',
-      '@zxcvbn-ts/core',
-    ]
   }
 })
