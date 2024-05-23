c374e21fccbd5a2da37efc062f5b62fb3980a623
ShanYi-Hui <shanyi.hui@qq.com>
2024-05-23 03:53:33 +0000

    Deploying to gh-pages-gitee from @ syh-micro-build/mb-admin@eadadd208ef62a672deb00fc8e612468028b645b 🚀

--- assets/index-CQ3c3Wpt.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-8p8FoHEo.js";const t=()=>e.get({url:"/mock/role/table"});export{t as g};
---
+++ assets/index-DBOZ50jr.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-GkpvI__y.js";const r=()=>t.get({url:"/mock/menu/list"});export{r as g};
