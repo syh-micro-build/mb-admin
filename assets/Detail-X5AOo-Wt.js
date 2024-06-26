65c6905191ed6bfe9041a14883276f50ff06f7b4
ShanYi-Hui <shanyi.hui@qq.com>
2024-06-26 01:29:53 +0000

    Deploying to gh-pages-gitee from @ syh-micro-build/mb-admin@3b8f425508d7f20005a83d0a958061a30ebaf14e 🚀

---
+++ assets/Detail-X5AOo-Wt.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Detail.vue_vue_type_script_setup_true_lang-CdHHXKlu.js";import"./Descriptions-BNDdkkkE.js";import"./vue-chunks-DOWhatC1.js";import"./index-4pidxUvY.js";import"./element-plus-6v3c1uXT.js";/* empty css                  *//* empty css               */import"./Icon-BSmdJKT1.js";export{o as default};
