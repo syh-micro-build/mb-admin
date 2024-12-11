4bb04db327d0df72a22f1c9b2a8e59d57870c6bb
ShanYi-Hui <shanyi.hui@qq.com>
2024-12-11 08:52:46 +0000

    Deploying to gh-pages from @ syh-micro-build/mb-admin@6441b2197a16594cb9e7ecc5c3b4a81685f93a12 🚀

--- assets/Detail-Cn2QuSjS.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Detail.vue_vue_type_script_setup_true_lang-C7c7VReT.js";import"./Descriptions-CzJeaLh7.js";import"./vue-chunks-C8vH-0tq.js";import"./index-DLMkugkx.js";import"./element-plus-BqdG1kF4.js";/* empty css                  *//* empty css               */import"./Icon-LrwZ7kOU.js";export{o as default};
--- assets/Detail.vue_vue_type_script_setup_true_lang-C3g5luqI.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./Descriptions-CzJeaLh7.js";import{q as t,t as r,S as c,u as n}from"./vue-chunks-C8vH-0tq.js";const u=t({__name:"Detail",props:{currentRow:{type:Object,default:()=>null},detailSchema:{type:Array,default:()=>[]}},setup(e){return(o,s)=>(r(),c(n(a),{schema:e.detailSchema,data:e.currentRow||{}},null,8,["schema","data"]))}});export{u as _};
