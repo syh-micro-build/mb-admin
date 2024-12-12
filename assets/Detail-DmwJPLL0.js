360302ced8412e700b3f2f3463ba70968ab3c7d6
ShanYi-Hui <shanyi.hui@qq.com>
2024-12-12 10:35:20 +0000

    Deploying to gh-pages-gitee from @ syh-micro-build/mb-admin@2030d7c1fdd0f48be63b97af80d544d71eac4ae9 🚀

--- assets/Detail-DmwJPLL0.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Detail.vue_vue_type_script_setup_true_lang-CMI7Y1Ga.js";import"./Descriptions-C40RDjHI.js";import"./vue-chunks-ChjHxF1G.js";import"./index-BECrzDib.js";import"./element-plus-CUBv5pjL.js";/* empty css                  *//* empty css               */import"./Icon-AxGGwV-J.js";export{o as default};
---
+++ assets/index-BTKQJI9X.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-CKBT0Z4D.js";const r=()=>t.get({url:"/mock/menu/list"});export{r as g};
