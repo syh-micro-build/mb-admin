70f645c9dd7b6c6b7e2b04bbc92fd9e6077e7c9f
ShanYi-Hui <shanyi.hui@qq.com>
2024-12-13 02:47:54 +0000

    Deploying to gh-pages from @ syh-micro-build/mb-admin@5db1c4e8b89a77a80fae4e9eae15f764d768e7c8 🚀

--- assets/Detail-CumDeH1C.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Detail.vue_vue_type_script_setup_true_lang-DcpzuKSD.js";import"./Descriptions-CopuwYlW.js";import"./vue-chunks-ChjHxF1G.js";import"./index-CiB76NFm.js";import"./element-plus-CUBv5pjL.js";/* empty css                  *//* empty css               */import"./Icon-DfDcjZQU.js";/* empty css               */export{o as default};
--- assets/index-BKStt59-.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-CiB76NFm.js";const t=()=>e.get({url:"/mock/role/table"});export{t as g};
